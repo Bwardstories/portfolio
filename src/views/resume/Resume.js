e73ac0ed10892834007edabb5d1467e9268b99c7
Bwardstories <thewardbunch@gmail.com>
2021-09-03 15:20:43 -0400

    is it working

--- src/views/resume/Resume.js
+++ src/views/resume/Resume.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Viewer } from "@react-pdf-viewer/core";
-import pdfFile from "../../assets/resume/Resume083121.pdf";
+import pdfFile from "../../assets/resume/Resume082721.pdf";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 import { Worker } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";

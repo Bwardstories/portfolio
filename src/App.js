cd91b2b4d67f23c83c3040716f1983be83535c89
Bwardstories <thewardbunch@gmail.com>
2021-09-04 20:18:19 -0400

    trying to fix problem with github

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import { Switch, Route } from "react-router";
 import Header from "./components/header/Header";
@@ -7,11 +8,16 @@ import Resume from "./views/resume/Resume";
 import { useProvideContact } from "./hooks";
 import "./App.scss";
 import SocialLinks from "./components/socialLinks/SocialLinks";
+=======
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> fd49a13 (added server)
 
 function App() {
   const { state } = useProvideContact();
   console.log(state.contactFormVisible);
   return (
+<<<<<<< HEAD
     <div className="mainAppContainer">
       <Header />
       {state.contactFormVisible ? <ContactForm /> : ""}
@@ -20,6 +26,23 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route path="/resume" component={Resume} />
       </Switch>
+=======
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> fd49a13 (added server)
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
@@ -13,6 +14,22 @@ ReactDOM.render(
         <App />
       </BrowserRouter>
     </ProvideContact>
+=======
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+>>>>>>> fd49a13 (added server)
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
